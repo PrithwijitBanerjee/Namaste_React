f4583c280862d2a60ce4ea9f3895496221140819
Prithwijit_Banerjee <prithwijit98@gmail.com>
2024-04-12 15:29:59 +0530

    This is commit about laying out the foundation of react

--- app.js
+++ app.js
@@ -25,18 +25,85 @@ import ReactDOM from "react-dom/client";
 // 
 
 
+// React Element without  JSX ...
+// const mainDivContainer = React.createElement('div', { className: 'container' },
+//     [React.createElement('div', { id: 'first-container', key: 1 },
+//         React.createElement('h1', { id: 'first-header' }, 'First Container Element')),
+//     React.createElement('div', { id: 'second-container', key: 2 },
+//         React.createElement('h1', { id: 'second-header' }, 'Second Container Element'))]);
 
-const mainDivContainer = React.createElement('div', { className: 'container' },
-    [React.createElement('div', { id: 'first-container', key: 1 },
-        React.createElement('h1', { id: 'first-header' }, 'First Container Element')),
-    React.createElement('div', { id: 'second-container', key: 2 },
-        React.createElement('h1', { id: 'second-header' }, 'Second Container Element'))]);
+// React Element with JSX ...
+// JSX ===> React Element (by Babel Transplier) ===> JS Object ====> html tag (by render() method)
+// const mainDivContainer2 = (<div className="container">
+//     <div id="first-container">
+//         <h1 id="first-header" className="head">
+//             First Container JSX
+//         </h1>
+//     </div>
+//     <div id="second-container">
+//         <h1 id="second-header" className="head">
+//             Second Container JSX
+//         </h1>
+//     </div>
+// </div>)
 
-
-
+// React Component with JSX ....(Component name always should be starts with Capital letter)
+// React Functional Component (Stateless Component) ===> Normal JS Function with First letter capital ...
+const Container = () => {
+    return (<>
+            <h1>this is arrow functional component ...</h1>
+    </>);
+}
 // console.log(divContainer.props.children);
+
+
+// React Element inside React Component ...
+// const Container = () => {
+//     return (<>
+//             <h1>this is arrow functional component ...</h1>
+//             {mainDivContainer2}
+//     </>);
+// }
+
+// React Component inside React Element ...
+
+// const mainDivContainer2 = (<div className="container">
+//     <div id="first-container">
+//         <h1 id="first-header" className="head">
+//             First Container JSX
+//         </h1>
+//         <Container/>
+//     </div>
+//     <div id="second-container">
+//         <h1 id="second-header" className="head">
+//             Second Container JSX
+//         </h1>
+//     </div>
+// </div>)
+
+
+// Page Become freeze when ...
+
+//React Class Component ...
+class ReactComponent extends React.Component {
+    render() {
+        return (<>
+            <h1>this is react class component</h1>
+            {reactElement}
+        </>)
+    }
+}
+
+var reactElement = (<div>
+    <h1>this is react element</h1>
+    <ReactComponent />
+</div>);
+
+// Babel does sanitization of API data api.getUsers()...
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // console.log(root);
 
-root.render(mainDivContainer); 
+// root.render() ==> converts React Element into html tag and renders it to actual screen (real DOM)
+root.render(<Container />); 
